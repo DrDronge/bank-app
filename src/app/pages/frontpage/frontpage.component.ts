31b110fd6a6393e1559ea127fc1e59752ba1d140
DrDronge <mdrongesen12@gmail.com>
2025-12-16 18:57:41 +0100

    Okay

---
+++ src/app/pages/frontpage/frontpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontpage',
+  imports: [],
+  templateUrl: './frontpage.component.html',
+  styleUrl: './frontpage.component.scss'
+})
+export class FrontpageComponent {
+
+}
